e079622596a23386e25f0cff7db7dcc9933d1800
Nick Shvelidze <shveloo@gmail.com>
2012-09-20 11:07:28 +0400

    Removed temporary build files

--- build/lightwinter-1.0/debian/lightwinter.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the lightwinter package
-#
-0 4	* * *	root	[ -x /usr/bin/lightwinter_maintenance ] && /usr/bin/lightwinter_maintenance
--- build/lightwinter-1.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lightwinter):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="lightwinter" command="/usr/bin/lightwinter"
